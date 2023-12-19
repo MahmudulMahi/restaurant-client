b93e0f3a174586de4da8cfe6c6d94f6df6372165
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-12-19 13:14:32 +0600

    add dynamic titel sectionn

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -9,17 +9,23 @@ import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const Category = () => {
   return (
-    <Swiper
+ <section>
+<SectionTitle
+subHeading={"From 11.00 am to 10.00pm"}
+heading={"Order Online"}
+></SectionTitle>
+<Swiper
     slidesPerView={3}
-    spaceBetween={1}
+    spaceBetween={30}
     pagination={{
       clickable: true,
     }}
     modules={[Pagination]}
-    className="mySwiper"
+    className="mySwiper mb-6"
   >
     <SwiperSlide>
       <img className='w-[426px]' src={slide1} alt="" />
@@ -39,6 +45,7 @@ const Category = () => {
     </SwiperSlide>
  
   </Swiper>
+ </section>
   );
 };
 
