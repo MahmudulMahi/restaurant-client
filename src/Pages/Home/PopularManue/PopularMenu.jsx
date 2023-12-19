5d705b838e70db82b74293ac9f995e15bb18813f
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-12-19 13:46:25 +0600

    add menu  sectionn

--- src/Pages/Home/PopularManue/PopularMenu.jsx
+++ src/Pages/Home/PopularManue/PopularMenu.jsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 
 const PopularMenu = () => {
+
+  const [menu,setMenu]=useState([])
+
+  useEffect(()=>{
+    fetch('/menu.json')
+    .then(res=>res.json())
+    .then(data=>{
+      const popularItems=data.filter(item=>item.category ==='popular')
+      setMenu(popularItems)})
+  },[])
   return (
     <section>
       <SectionTitle
       heading="From Our Menu"
       subHeading="Popular Items"
       ></SectionTitle>
+
+      <div>
+        {
+          menu.map(item=>)
+        }
+      </div>
     </section>
   );
 };
