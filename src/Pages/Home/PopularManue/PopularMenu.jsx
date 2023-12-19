5c9e7112b2e128f4f6775ee9d513043f41ec75e7
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-12-19 23:01:48 +0600

    fixed responsive

--- src/Pages/Home/PopularManue/PopularMenu.jsx
+++ src/Pages/Home/PopularManue/PopularMenu.jsx
@@ -21,7 +21,7 @@ const PopularMenu = () => {
       subHeading="Popular Items"
       ></SectionTitle>
 
-      <div className="grid md:grid-cols-2 gap-4">
+      <div className="grid md:grid-cols-2 gap-10">
         {
           menu.map(item=> <MenuItem
           key={item._id}
---
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
+const Testimonials = () => {
+  return (
+    <section>
+      <SectionTitle
+      subHeading={'What our Client Say'}
+      heading={'Testimonials'}
+      ></SectionTitle>
+    </section>
+  );
+};
+
+export default Testimonials;
