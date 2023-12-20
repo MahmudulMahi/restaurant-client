1150d710fca55b8c6a1ff802599080e40ba14a90
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-12-20 12:08:23 +0600

    add helmet

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,10 +4,15 @@ import Category from '../Category/Category';
 import PopularMenu from '../PopularManue/PopularMenu';
 import Featured from '../Featured/Featured';
 import Testimonials from '../Testimonials/Testimonials';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
   return (
     <div>
+        <Helmet>
+        <title>Restaurant | Home</title>
+        {/* <link rel="canonical" href="https://www.tacobell.com/" /> */}
+      </Helmet>
       <Banner></Banner>
       <Category></Category>
       <PopularMenu></PopularMenu>
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,8 @@
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
-  const navOptions = <>
-
-  </>
+ 
   return (
     <>
       <div className="navbar fixed z-30 bg-opacity-25 bg-black text-white max-w-screen-xl">
@@ -13,7 +12,8 @@ const Navbar = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Item 1</a></li>
+            <li><Link to='/'>Home</Link></li>
+              <li><Link to='/menu'>Our Menu</Link></li>
               <li>
                 <a>Parent</a>
                 <ul className="p-2">
@@ -28,7 +28,8 @@ const Navbar = () => {
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/menu'>Our Menu</Link></li>
             <li>
               <details>
                 <summary>Parent</summary>
