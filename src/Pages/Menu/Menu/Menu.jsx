7f15c79d748d745d671e7baa149956211a1119e0
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-12-20 12:59:22 +0600

    add parallex

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import Banner from '../../Home/Banner/Banner';
 
 const Menu = () => {
   return (
     <div>
+      
       <Helmet>
         <title>Restaurant | Menu</title>
         {/* <link rel="canonical" href="https://www.tacobell.com/" /> */}
       </Helmet>
-      <h2>menu</h2>
+      <Cover img={menuImg} title="our menu"></Cover>
     </div>
   );
 };
