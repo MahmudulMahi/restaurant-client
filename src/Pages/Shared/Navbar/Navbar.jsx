a818411453a62e80992ec0cfc27f3780b7aada90
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-12-21 22:31:23 +0600

    add all manu dynamicly

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -31,6 +31,7 @@ const Navbar = () => {
           <ul className="menu menu-horizontal px-1">
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/menu'>Our Menu</Link></li>
+          <li><Link to='/order'>Order Food</Link></li>
             <li>
               <details>
                 <summary>Parent</summary>
