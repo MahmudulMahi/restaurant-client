dc17f6c41f3c7dacd5c4258f39ba1222e9a6e102
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-12-21 23:28:39 +0600

    add all manu dynamicly

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -9,9 +9,12 @@ import OrderTab from '../OrderTab/OrderTab';
 import { useParams } from 'react-router-dom';
 
 const Order = () => {
+  const categories=['salad ','pizza','soup','dessert','drinks']
+  const {category}=useParams();
+  const initialIndex=categories.indexOf(category)
   const [tabIndex,setTabIndex]=useState(0)
   const [menu] = useMenu();
-  const {category}=useParams();
+  
   console.log(category)
   const desserts=menu.filter(item=>item.category ==='dessert');
   const soup=menu.filter(item=>item.category ==='soup');
