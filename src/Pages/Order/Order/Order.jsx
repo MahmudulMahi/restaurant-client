35689605dcf3e45fc3ac96b5562b28b9c52ed10b
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-12-21 16:07:46 +0600

    add all manu dynamicly

---
+++ src/Pages/Order/Order/Order.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import orderCoverImg from '../../Shared/Cover'
+import Cover from '../../Shared/Cover/Cover';
+
+const Order = () => {
+  return (
+    <div>
+      <Cover img={orderCoverImg} title="Order Food"></Cover>
+    </div>
+  );
+};
+
+export default Order;
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             <li><Link to='/'>Home</Link></li>
               <li><Link to='/menu'>Our Menu</Link></li>
+              <li><Link to='/order'>Order Food</Link></li>
               <li>
                 <a>Parent</a>
                 <ul className="p-2">
