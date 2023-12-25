d99b77b9d9e7215b50660f3ac531a15e090f3a59
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-12-25 23:09:53 +0600

    add donate blood page

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -10,11 +10,13 @@ import { useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
 const Order = () => {
-  const categories=['salad ','pizza','soup','dessert','drinks']
+  
+  const categories=['salad','pizza','soup','dessert','drinks']
   const {category}=useParams();
   const initialIndex=categories.indexOf(category)
   const [tabIndex,setTabIndex]=useState(initialIndex)
   const [menu] = useMenu();
+  
   
   console.log(category)
   const desserts=menu.filter(item=>item.category ==='dessert');
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -8,13 +8,14 @@ const Navbar = () => {
       <div className="navbar fixed z-30 bg-opacity-25 bg-black text-white max-w-screen-xl">
         <div className="navbar-start">
           <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <div className="btn btn-ghost lg:hidden" tabIndex={0} role="button" >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             <li><Link to='/'>Home</Link></li>
               <li><Link to='/menu'>Our Menu</Link></li>
               <li><Link to='/order/salad'>Order Food</Link></li>
+              <li><Link to='/login'>Login</Link></li>
               <li>
                 <a>Parent</a>
                 <ul className="p-2">
@@ -32,6 +33,7 @@ const Navbar = () => {
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/menu'>Our Menu</Link></li>
           <li><Link to='/order/salad'>Order Food</Link></li>
+          <li><Link to='/login'>Login</Link></li>
             <li>
               <details>
                 <summary>Parent</summary>
