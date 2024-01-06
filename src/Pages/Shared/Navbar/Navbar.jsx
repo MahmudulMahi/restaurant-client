082ad35d8b0e7d4f6229da0df0294a1e703565fc
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-01-07 01:07:18 +0600

    react hook form

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
+import { BsCart } from "react-icons/bs";
 
 
 const Navbar = () => {
-  const {user,logOut}=useContext(AuthContext)
+  const { user, logOut } = useContext(AuthContext)
 
-  const handleLogOut=()=>{
+  const handleLogOut = () => {
     logOut()
-    .then(()=>{})
-    .catch(error=>console.log(error))
+      .then(() => { })
+      .catch(error => console.log(error))
   }
 
 
- 
+
   return (
     <>
       <div className="navbar fixed z-30 bg-opacity-25 bg-black text-white max-w-screen-xl">
@@ -23,45 +24,54 @@ const Navbar = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-          <>
-          <li><Link to='/'>Home</Link></li>
-              <li><Link to='/menu'>Our Menu</Link></li>
-              <li><Link to='/order/salad'>Order Food</Link></li>
-              <li><Link to='/secret'>Secret</Link></li>
-          
-          
-              {
-                user?<>
-                <li onClick={handleLogOut} className="btn btn-ghost">Log Out</li>
-                </>:<>
-                <li><Link to='/login'>Login</Link></li>
-                </>
-              }
-          </>
-              
-              
+              <>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/menu'>Our Menu</Link></li>
+                <li><Link to='/order/salad'>Order Food</Link></li>
+                <li><Link to='/secret'>Secret</Link></li>
+
+
+                {
+                  user ? <>
+                    <span className="text-white" >{user?.displayName}</span>
+                    <li onClick={handleLogOut} className="btn btn-ghost">Log Out</li>
+                  </> : <>
+                    <li><Link to='/login'>Login</Link></li>
+                  </>
+                }
+              </>
+
+
             </ul>
           </div>
           <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
         <div className="navbar-center hidden lg:flex  ">
           <ul className="menu menu-horizontal px-1">
-          <>
-          <li><Link to='/'>Home</Link></li>
+            <>
+              <li><Link to='/'>Home</Link></li>
               <li><Link to='/menu'>Our Menu</Link></li>
               <li><Link to='/order/salad'>Order Food</Link></li>
               <li><Link to='/secret'>Secret</Link></li>
-              
-          
-          
+
+              <li>
+                <Link to="/">
+                  <button className="btn">
+                  <BsCart />
+                    <div className="badge badge-secondary">+99</div>
+                  </button>
+                </Link>
+              </li>
+
               {
-                user?<>
-                <li onClick={handleLogOut} className="btn  btn-ghost">Log Out</li>
-                </>:<>
-                <li><Link to='/login'>Login</Link></li>
+                user ? <>
+                  <span className="text-white" >{user?.displayName}</span>
+                  <li onClick={handleLogOut} className="btn  btn-ghost">Log Out</li>
+                </> : <>
+                  <li><Link to='/login'>Login</Link></li>
                 </>
               }
-          </>
+            </>
           </ul>
         </div>
         <div className="navbar-end">
--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+
+import Swal from 'sweetalert2';
+import useAuth from '../../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const FoodCard = ({item}) => {
   const {name,image,price,recipe}=item
+
+  const {user}=useAuth()
+  const navigate=useNavigate()
+
+  const handelAddToCart =food=>{
+    if(user && user.email){
+      // send database
+    }
+    else{
+      Swal.fire({
+        title: "you are not login",
+        text: "Please login add to cart",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, Login"
+      }).then((result) => {
+        if (result.isConfirmed) {
+         navigate('/login')
+        }
+      });
+    }
+  }
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
     <figure><img src={image} alt="Shoes" /></figure>
@@ -10,7 +37,7 @@ const FoodCard = ({item}) => {
       <h2 className="card-title">{name}</h2>
       <p>{recipe}</p>
       <div className="card-actions justify-end">
-        <button className="btn btn-primary">Add to Cart</button>
+        <button onClick={()=> handelAddToCart(item)} className="btn btn-primary">Add to Cart</button>
       </div>
     </div>
   </div>
