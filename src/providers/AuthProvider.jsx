3d33cf1c713bb80e2d8ea34fe0a0c4f5649e6186
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-01-09 22:09:50 +0600

    create schedule route

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,72 @@
+
+import  {  createContext,  useEffect, useState } from 'react';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { app } from '../firebase/firebase.config';
+const auth = getAuth(app);
+export const AuthContext=createContext(null)
+const AuthProvider=({children})=>{
+
+  const [user ,setUser]=useState(null)
+  const [loading,setLoading]=useState(true)
+  const googleProvider=new GoogleAuthProvider();
+
+  const createuser = (email,password)=>{
+    setLoading(true)
+    return createUserWithEmailAndPassword(auth,email,password)
+  }
+
+  const signIn=(email,password)=>{
+    setLoading(true)
+    return signInWithEmailAndPassword(auth,email,password)
+  }
+
+  const googleSignIn=()=>{
+    setLoading(true)
+    return signInWithPopup(auth, googleProvider)
+  }
+
+  const logOut=()=>{
+    setLoading(true)
+    return signOut(auth)
+  }
+
+  const updateUserProfile =(name,photo)=>{
+    return updateProfile(auth.currentUser, {
+      displayName: name, photoURL: photo
+    })
+    
+  }
+
+  useEffect(()=>{
+    const unsubscribe = onAuthStateChanged(auth,currentUser =>{
+      setUser(currentUser)
+      console.log('user',currentUser)
+      if(currentUser){
+
+      }
+      else{
+        // do something
+      }
+      setLoading(false)
+    })
+    return () =>{
+      return unsubscribe
+    }
+  },[])
+  const authInfo={
+    user,
+    loading,
+    createuser,
+    signIn,
+    logOut,
+    updateUserProfile,
+    googleSignIn
+  }
+  return (
+    <AuthContext.Provider value={authInfo}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
